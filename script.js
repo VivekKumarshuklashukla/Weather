facc5d1d9665f9b89a77d84559e2d9bd9486ce42
Vivekanand Shukla 420 <primevivek14@gmail.com>
2024-10-25 22:25:48 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'fcef4035d5msh388c10a4f16c664p1391cdjsn2ee59e4d58de',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
